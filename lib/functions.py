a7be4c89fab7268ba29ae283a6fe4d3b77e8fc8f
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-27 13:34:26 +0100

    All passed

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,27 @@
 
 def greet_programmer():
     pass
+    print(f"Hello, programmer!")
 
-def greet(name):
+def greet(name= "Alfred"):
     pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
+def greet_with_default(name= "programmer"):
     pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
+def add(num1= 45, num2= 55):
     pass
+    return num1 + num2
 
-def halve(number):
+
+def halve(number= 98):
     pass
+    return number / 2
+
+def halve(number= 99.0):
+    pass
+    return number / 2
+
+
